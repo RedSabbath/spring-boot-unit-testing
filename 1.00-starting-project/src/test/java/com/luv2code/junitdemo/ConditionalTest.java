eacd7944ee1daeb21e50b7e6f6b6221675c9fe9c
Dustin <dustin.bouse@gmail.com>
2026-07-22 15:00:39 -0700

    created conitional unit test examples for environmental variables and system properties

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -3,8 +3,7 @@ package com.luv2code.junitdemo;
 import jdk.jfr.Enabled;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
 
 public class ConditionalTest {
 
@@ -23,6 +22,18 @@ public class ConditionalTest {
     @Test
     @EnabledOnOs(OS.WINDOWS)
     void testForWindows()  {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named="LUV2CODE_ENV", matches = "DEV")
+    void testOnlyForDevEnvironment()  {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "LUV2CODE", matches = "CI_CD_DEPLOY")
+    void testOnlyForSystemProperties()  {
         // execute method and perform asserts
     }
 
