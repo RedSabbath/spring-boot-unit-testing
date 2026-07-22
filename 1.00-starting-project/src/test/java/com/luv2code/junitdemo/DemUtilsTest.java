6440e17002c3de3ed07cafd5abaa00f8203d448d
Dustin <dustin.bouse@gmail.com>
2026-07-22 11:15:20 -0700

    created unit test for isGreater function

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemUtilsTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+//@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class DemUtilsTest {
 
     DemoUtils demoUtils;
@@ -11,38 +13,24 @@ class DemUtilsTest {
     @BeforeEach
     void setupBeforeEach()  {
         demoUtils = new DemoUtils();
-        System.out.println("@BeforeEach executes before the execution of each test method");
-    }
-
-    @AfterEach
-    void tearDownAfterEach()    {
-        System.out.println("Running @AfterEach");
-        System.out.println();
-    }
-
-    @BeforeAll
-    static void setupBeforeEachClass()  {
-        System.out.println("@BeforeAll executes only once before all test methods executing in the class");
-    }
-
-    @AfterAll
-    static void tearDownAfterAll()  {
-        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+//        System.out.println("@BeforeEach executes before the execution of each test method");
     }
 
     @Test
+    @DisplayName("Equals and Not Equals")
     void testEqualsAndNotEquals() {
 
-        System.out.println("Running test: testEqualsAndNotEquals");
+//        System.out.println("Running test: testEqualsAndNotEquals");
 
         assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
     }
 
+    @DisplayName("Null and Not Null")
     @Test
-    void testNullAndNotNull()   {
+        void testNullAndNotNull()   {
 
-        System.out.println("Running test: testNullAndNotNull");
+//        System.out.println("Running test: testNullAndNotNull");
 
         String str1 = null;
         String str2 = "luv2code";
@@ -50,5 +38,46 @@ class DemUtilsTest {
         assertNull(demoUtils.checkNull(str1), "Object should be null");
         assertNotNull(demoUtils.checkNull(str2), "Object should NOT be null");
     }
+
+    @DisplayName("True and False")
+    @Test
+        void testTrueFalse()    {
+            int gradeOne = 10;
+            int gradeTwo = 5;
+
+            assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+            assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @DisplayName("Same and Not Same")
+    @Test
+    void testSameAndNotSame()  {
+        String str = "luv2code";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to the same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer to the same object");
+    }
+
+
+
+
+//    @AfterEach
+//    void tearDownAfterEach()    {
+//        System.out.println("Running @AfterEach");
+//        System.out.println();
+//    }
+//
+//    @BeforeAll
+//    static void setupBeforeEachClass()  {
+//        System.out.println("@BeforeAll executes only once before all test methods executing in the class");
+//    }
+//
+//    @AfterAll
+//    static void tearDownAfterAll()  {
+//        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+//    }
+
+
+
 
 }
