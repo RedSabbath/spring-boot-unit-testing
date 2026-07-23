ece1b7d04aeacfa6588920433b332ee3f92c4488
Dustin <dustin.bouse@gmail.com>
2026-07-22 19:08:21 -0700

    created MainApp for FizzBuzz example problem to print results as requested

---
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/MainApp.java
@@ -0,0 +1,12 @@
+package com.luv2code.tdd;
+
+public class MainApp {
+
+    static void main() {
+        for (int i = 1; i <= 100; i++)  {
+            // System.out.println(i + "," + FizzBuzz.compute(i));
+            System.out.println(FizzBuzz.compute(i));
+        }
+    }
+
+}
