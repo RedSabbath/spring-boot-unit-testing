bab31b593928ca75949396c13064f13c2fd498fc
Dustin <dustin.bouse@gmail.com>
2026-07-22 17:05:47 -0700

    use or test driven development. created failing test first then implimented code to make test pass

---
+++ 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -0,0 +1,40 @@
+package com.luv2code.tdd;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class FizzBuzzTest {
+
+    // If number is divisible by 3              print Fizz
+    // If number is divisible by 5              print Buzz
+    // If number is divisible by 3 and 5        print FizzBuzz
+    // If number is NOT divisible by 3 or 5     print the number
+
+    // If number is divisible by 3              print Fizz
+    @Order(1)
+    @DisplayName("Divisible by Three")
+    @Test
+    void testForDivisibleByThree()  {
+
+        String expected = "Fizz";
+
+        assertEquals(expected, FizzBuzz.compute(3), "Should return Fizz");
+    }
+
+    // If number is divisible by 5              print Buzz
+    @Order(2)
+    @DisplayName("Divisible by Five")
+    @Test
+    void testForDivisibleByFive()  {
+
+        String expected = "Buzz";
+
+        assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
+    }
+
+
+
+}
