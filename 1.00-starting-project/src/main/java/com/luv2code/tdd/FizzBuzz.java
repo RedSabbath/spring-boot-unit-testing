525fcd664ce6fe1c511f1422731a6ebe3e982fa9
Dustin <dustin.bouse@gmail.com>
2026-07-22 17:55:47 -0700

    Refactored code. Showing use of test driven development

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -9,18 +9,37 @@ public class FizzBuzz {
 
     public static String compute(int i) {
 
-        if ( (i % 3 ==0) && (i % 5 == 0) )  {
-            return "FizzBuzz";
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0)    {
+            result.append("Fizz");
         }
-        if (i % 3 == 0)  {
-            return "Fizz";
+        if (i % 5 ==0)  {
+            result.append("Buzz");
         }
-        else if (i % 5 == 0)  {
-            return "Buzz";
+        if (result.isEmpty())   {
+            result.append(i);
         }
-        else {
-            return Integer.toString(i);
-        }
+
+        return result.toString();
+
     }
+
+
+//    public static String compute(int i) {
+//
+//        if ( (i % 3 ==0) && (i % 5 == 0) )  {
+//            return "FizzBuzz";
+//        }
+//        if (i % 3 == 0)  {
+//            return "Fizz";
+//        }
+//        else if (i % 5 == 0)  {
+//            return "Buzz";
+//        }
+//        else {
+//            return Integer.toString(i);
+//        }
+//    }
 
 }
