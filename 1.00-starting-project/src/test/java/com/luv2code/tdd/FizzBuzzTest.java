64fac3def9979ca73f1ac5d244e5209c7c68c63a
Dustin <dustin.bouse@gmail.com>
2026-07-22 17:13:52 -0700

    created tests for all outputs for FizzBuzz scenario

--- 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -35,6 +35,27 @@ class FizzBuzzTest {
         assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
     }
 
+    // If number is divisible by 3 and 5        print FizzBuzz
+    @Order(3)
+    @DisplayName("Divisible by Three and Five")
+    @Test
+    void testForDivisibleByThreeAndFive()  {
+
+        String expected = "FizzBuzz";
+
+        assertEquals(expected, FizzBuzz.compute(15), "Should return FizzBuzz");
+    }
+
+    // If number is NOT divisible by 3 or 5     print the number
+    @Order(4)
+    @DisplayName("Not Divisible by Three or Five")
+    @Test
+    void testForNotDivisibleByThreeOrFive()  {
+
+        String expected = "1";
+
+        assertEquals(expected, FizzBuzz.compute(1), "Should return 1");
+    }
 
 
 }
